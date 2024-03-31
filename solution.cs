de9bf0b9c729e7f71ebc82522b41fca58deb8c9a
Kooreis <kooreis.official@gmail.com>
2024-03-31 09:31:32 -0500

    Initialized matrixA with 3 rows and 3 columns.

--- solution.cs
+++ solution.cs
@@ -1,14 +1,1 @@
-function matrixMultiplication(matrixA, matrixB) {
-    let result = [];
-    for(let i = 0; i < matrixA.length; i++) {
-        result[i] = [];
-        for(let j = 0; j < matrixB[0].length; j++) {
-            let sum = 0;
-            for(let k = 0; k < matrixA[0].length; k++) {
-                sum += matrixA[i][k] * matrixB[k][j];
-            }
-            result[i][j] = sum;
-        }
-    }
-    return result;
-}
+let matrixA = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
